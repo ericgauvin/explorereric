efd8adf23ef0f437d7e65dbb0c3bd97b0862c7b5
Eric Gauvin <eric.j.gauvin@gmail.com>
2018-01-25 16:46:42 -0500

    modified:   Gemfile.lock 	modified:   app/assets/stylesheets/application.css 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/photos/index.html.erb 	modified:   app/views/photos/show.html.erb 	modified:   app/views/welcome/index.html.erb 	modified:   config/routes.rb 	app/assets/images/background.jpg 	app/assets/javascripts/countries.coffee 	app/assets/stylesheets/countries.scss 	app/controllers/countries_controller.rb 	app/helpers/countries_helper.rb 	app/views/countries/ 	app/views/photos/index.html.erb.OLD 	test/controllers/countries_controller_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-
+  get '/countries/countries', :as => 'countries_page'
+  
   resources :photos
 
   root 'welcome#index'
+  root 'countries#show', countries: 'countries'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
