09d68cdb235c0dee1deadae8f9e029f2bda7b073
Eric Gauvin <eric.j.gauvin@gmail.com>
2020-01-02 22:15:12 -0500

    change to posgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180224203145) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
